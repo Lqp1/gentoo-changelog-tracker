1e57dce004e3439dedda54f2142ed7d93df419d1
Thomas Langé <thomas.lange.oss@gmail.com>
2023-03-25 10:26:23 +0100

    Add simple completion command for the tool

---
+++ sh/gentoo-changelog-tracker-completion.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+_gentoo_changelog_tracker_complete() {
+  export EIX_LIMIT=0
+  local cur prev opts packages
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+  if [[ "$cur" == */* ]]; then
+      cat="${cur%%/*}"
+      atom="${cur#*/}"
+  else
+      atom=""
+      cat="$cur"
+  fi
+
+  echo "${cat} / ${atom} ==" >> /tmp/toto
+  packages=$(eix -#n -C "$cat" "${atom}" | awk '{print $1}' | sort)
+
+  if [[ ${prev} == "gentoo-changelog-tracker" ]]; then
+    COMPREPLY=( $(compgen -A package -W "${packages}") )
+  fi
+  return 0
+}
+
+complete -F _gentoo_changelog_tracker_complete gentoo-changelog-tracker
